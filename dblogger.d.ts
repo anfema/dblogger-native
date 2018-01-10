585dadca914830cb169656dc6cdeef53c6884e1f
André Wachter <a.wachter@anfe.ma>
2018-01-10 14:03:13 +0100

    Update TypeScript definitions
    - Convert LogLevel to const enum
    - Add a dummy enum, so tsc will emit "require('dblogger')" statements

--- dblogger.d.ts
+++ dblogger.d.ts
@@ -1,7 +1,12 @@
-// http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html
+// template: http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html
 
 declare namespace dblogger {
-	export enum LogLevel {
+	enum Dummy {
+		// This is necessary so TS emits a require statement, since all other types in this
+		// namespace are compiled away.
+	}
+
+	export const enum LogLevel {
 		Trace = 10,
 		Debug = 20,
 		Info = 30,
